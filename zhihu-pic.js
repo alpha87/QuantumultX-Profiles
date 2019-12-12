2367a664f2e3cc9b74d9465abe9570a5a333b2af
简讯 <jianxun2004@outlook.com>
2019-12-12 17:43:11 +0800

    Update zhihu-pic.js

--- zhihu-pic.js
+++ zhihu-pic.js
@@ -1,4 +1,4 @@
 var obj = JSON.parse($response.body);
 obj.content = "尹俊勋你个大骗子！！！";
 $done({body: JSON.stringify(obj)}); 
-$notify("title", $request.url, htmlDoc);
+$notify("title", $request.url, $response.body);
