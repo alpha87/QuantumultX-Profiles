8122c35cc8426629e8a9a8217625191e6b74d276
简讯 <jianxun2004@outlook.com>
2019-12-12 17:41:29 +0800

    Update zhihu-pic.js

--- zhihu-pic.js
+++ zhihu-pic.js
@@ -1,24 +1,4 @@
-var htmlDoc = $response.body;
-
-/*
-// 匹配 content .
-var regex_html = /"content":.*?"collapsedBy":/g;
-match_result = htmlDoc.match(regex_html)[0]
-
-// 提取 content 的图片部分
-var regex_match = /\\u003Cfigure\\u003E\\.*?\\u003C\\u002Ffigure\\u003E/g;
-content_result = match_result.match(regex_match)
-
-// 使用 <br /> 拼接
-var result = content_result.join("\u003Cbr\u002F\u003E")
-
-var regex_html = /"content":.*?"collapsedBy":/g;
-match_result = doc.replace(regex_html, `"content": ${result}, "collapsedBy":`);
-$done({body: match_result});
-*/
-
-console.info("-------------------------");
-console.info(htmlDoc);
+var obj = JSON.parse($response.body);
+obj.content = "尹俊勋你个大骗子！！！";
+$done({body: JSON.stringify(obj)}); 
 $notify("title", $request.url, htmlDoc);
-
-
