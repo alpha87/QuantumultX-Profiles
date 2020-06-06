c2e63282ac77afd7f660f0ecb83cb9da841ef94d
简讯 <jianxun2004@outlook.com>
2020-06-06 12:09:48 +0800

    简化IT之家去广告代码

--- ITHome.js
+++ ITHome.js
@@ -1,40 +1,18 @@
-/**
- * @supported 
- */
-
 function indexAD(obj) {
   // 去除 IT 之家首页滚动板块广告
-  let del_num = [];
-  for (item of obj) {
-    if (item["isad"]) {
-      del_num.unshift(obj.indexOf(item));
-    }
-  }
-  for (n of del_num) {
-    obj.splice(n, 1);
-  }
-  return obj;
+  return obj.filter(item => !(item["isad"]));
 }
 
 function newslistAD(obj) {
   // 去除 IT 之家新闻列表广告
   let data = obj["newslist"];
-  let del_num = [];
-  for (item of data) {
-    if ("aid" in item) {
-      del_num.unshift(data.indexOf(item));
-    }
-  }
-  for (n of del_num) {
-    data.splice(n, 1);
-  }
-  obj["newslist"] = data;
+  obj["newslist"] = data.filter(item => !("aid" in item));
   return obj;
 }
 
 let obj = JSON.parse($response.body);
-if (obj["newslist"] == undefined) {
-  $done({ body: JSON.stringify(indexAD(obj)) });
-} else {
+if (obj["newslist"]) {
   $done({ body: JSON.stringify(newslistAD(obj)) });
+} else {
+  $done({ body: JSON.stringify(indexAD(obj)) });
 }
